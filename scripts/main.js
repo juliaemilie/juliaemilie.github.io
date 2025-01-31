36313ce3c0f232f34df4170f34f8867c4cc2402b
Julia Wuerthele <julia.wuerthele@gmx.de>
2025-01-31 20:13:31 +0100

    changes layout of table

--- scripts/main.js
+++ scripts/main.js
@@ -7,8 +7,6 @@ fetch("../src/emissions.json")
         let tableValues = "";
         for (let company of companies) {
             tableValues += "<tr><td>" + company.name + "</td><td>" + company.country + "</td><td>" + parseFloat(company.emissions).toFixed(3) + "</td></tr>";
-            console.log(company.name);
-            //tableValues += "<tr><td>" + company.name + "</td><td>" + company.country + "</td><td>" + company.emissions + "</td></tr>";
         }
         table.innerHTML = tableValues;
     })
